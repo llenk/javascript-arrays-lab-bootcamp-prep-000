ffbb887aae7f32437ef2356d91f2f81a0b93dace
Ellen Keal <ellenkeal@gmail.com>
2017-08-18 01:01:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ function destructivelyRemoveFirstKitten() {
 function prependKitten(name) {
   return [name, ...kittens];
 }
+function appendKitten(name) {
+  return [...kittens, name];
+}
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
